d18f28c86c3420bdf9e48678fec94b373728b712
puzzltest <puzzlr3@gmail.com>
2024-06-13 10:45:20 +0800

    temp

---
+++ temp.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+import { getDatabase, ref, set, onValue, get, update, increment, onDisconnect, runTransaction } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-database.js";
+import "https://cdn.jsdelivr.net/npm/zipson@latest/dist/zipson.min.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyCbkiYb_waAew2mLVr9ejn6FTGTr19Vi4A",
+  authDomain: "four--4.firebaseapp.com",
+  databaseURL: "https://four--4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "four--4",
+  storageBucket: "four--4.appspot.com",
+  messagingSenderId: "587842447388",
+  appId: "1:587842447388:web:bd3168656fc812f3e717d9",
+  measurementId: "G-9E0TRH93JC",
+};
+const params = new URLSearchParams(document.location.search);
+window.firebase = {};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+window.firebase.set = function(path, value) {
+  return set(ref(db, path), value);
+};
+window.firebase.get = function(path, getter_function) {
+  return onValue(ref(db, path), (snapshot) => {
+    getter_function(snapshot.val());
+  }, {
+    onlyOnce: true,
+  });
+};
+
+export const temp = {};
+temp.load = function() {
+  if (params.get("save")) {
+    window.firebase.get("/quad/" + params.get("save"), (data) => {
+      const raw = zipson.stringify(JSON.parse(data));
+      if (raw) {
+        localStorage.setItem("save", raw);
+        setTimeout(() => window.location.href = "/", 100);
+      }
+    });
+  }
+};
