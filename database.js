9b88eade660966e35658d4eda0a8b26ab824c29e
shootytest <shootytest@gmail.com>
2026-03-03 15:22:23 +0800

    oh no why is the room breaking

--- database.js
+++ database.js
@@ -22,7 +22,7 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 export const firebase = {};
 export const the_id = util.randletters(10);
-export const VERSION = 100301; // remember to change...
+export const VERSION = 100302; // remember to change...
 const version = VERSION;
 
 let already_ran_connect = false;
